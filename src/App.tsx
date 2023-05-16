3efb7aada5c087cd7bb98f388a7ebe91c0c6c647
seetsuko <radgjms@gmail.com>
2023-05-17 02:19:01 +0900

    style:header.css

--- src/App.tsx
+++ src/App.tsx
@@ -9,17 +9,6 @@ function App() {
       <Header />
       <div className="App">
         <AboutMe />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </div>
     </div>
   );
--- src/Header.tsx
+++ src/Header.tsx
@@ -4,14 +4,12 @@ export const Header = () => {
 
   return(
     <header className="App-header">
-      <div>
         <ul className="menu">
           <li>Home</li>
           <li>About Me</li>
           <li>Portfolio</li>
           <li>Skil</li>
         </ul>
-      </div>
     </header>
   )
 }
