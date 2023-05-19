0be8701bca87a2f75e1254dd64ffcac615f72830
seetsuko <radgjms@gmail.com>
2023-05-19 16:21:43 +0900

    feat:ホバー時にわかりやすいように色を変えた。

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,15 +1,68 @@
 import "./css/Header.css";
 import { Link as Scroll } from "react-scroll";
+import { Button } from "@mui/material";
 
 export const Header = () => {
 
   return(
     <header className="App-header">
         <ul className="menu">
-          <li><Scroll to="home" smooth>Home</Scroll></li>
-          <li><Scroll to="about-me" smooth>About Me</Scroll></li>
-          <li>Portfolio</li>
-          <li>Skil</li>
+          <li>
+            <Scroll to="home" smooth>
+            <Button
+                sx={{color: "#4f3737",
+                    fontSize: 60,
+                    marginBottom: 3,
+                    textTransform: "none",
+                    "&:hover": {
+                      background: "#e9dbaaa9"
+                    }
+                    }}>Home
+              </Button>
+            </Scroll>
+          </li>
+          <li>
+            <Scroll to="about-me" smooth>
+            <Button
+                sx={{color: "#4f3737",
+                    fontSize: 60,
+                    marginBottom: 3,
+                    textTransform: "none",
+                    "&:hover": {
+                      background: "#e9dbaaa9"
+                    }
+                    }}>About Me
+              </Button>
+            </Scroll>
+          </li>
+          <li>
+            <Scroll to="portfolio" smooth>
+              <Button
+                sx={{color: "#4f3737",
+                    fontSize: 60,
+                    marginBottom: 3,
+                    textTransform: "none",
+                    "&:hover": {
+                      background: "#e9dbaaa9"
+                    }
+                    }}>Portfolio
+              </Button>
+            </Scroll>
+          </li>
+          <li>
+            <Scroll to="skill" smooth>
+            <Button
+                sx={{color: "#4f3737",
+                    fontSize: 60,
+                    marginBottom: 3,
+                    textTransform: "none",
+                    "&:hover": {
+                      background: "#e9dbaaa9"
+                    }
+                    }}>Skill
+              </Button>
+            </Scroll>
+          </li>
         </ul>
     </header>
   );
